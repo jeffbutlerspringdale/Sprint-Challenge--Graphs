f0500fc7060aae41b185234158961951abf87f96
Jeff Butler <jeff.butler.springdale@gmail.com>
2020-02-21 12:56:02 -0600

    mpv

--- adv.py
+++ adv.py
@@ -25,9 +25,30 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
+visitedRooms = {}
+paths = []
+
+directions = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
+visitedRooms[player.current_room.id] = player.current_room.get_exits()
+
+while len(visitedRooms) < len(room_graph) - 1:
+
+    if player.current_room.id not in visitedRooms:
+        visitedRooms[player.current_room.id] = player.current_room.get_exits()
+        previous_direction = paths[-1]
+        visitedRooms[player.current_room.id].remove(previous_direction)
+
+
+    while len(visitedRooms[player.current_room.id]) == 0:
+        previous_direction = paths.pop()
+        traversal_path.append(previous_direction)
+        player.travel(previous_direction)
+
+    moveDirection = visitedRooms[player.current_room.id].pop(0)
+    traversal_path.append(moveDirection)
+    paths.append(directions[moveDirection])
+    player.travel(moveDirection)
 
 
 
@@ -36,8 +57,8 @@ visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
+for moveDirection in traversal_path:
+    player.travel(moveDirection)
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
@@ -51,12 +72,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
